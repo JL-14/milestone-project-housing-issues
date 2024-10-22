8f76c31b69f0d9a90eff6288e78d31eb6634555f
Jorgen Lovbakke <jorgenlovbakke@yahoo.co.uk>
2024-10-22 18:44:20 +0000

    Add first app page page_summary

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
+from app_pages import page_summary
+# from app_pages import page_descriptive_analytics
 
 # load pages scripts
 from app_pages.page_summary import page_summary_body
-from app_pages.page_churned_customer_study import page_churned_customer_study_body
-from app_pages.page_prospect import page_prospect_body
-from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_predict_churn import page_predict_churn_body
-from app_pages.page_predict_tenure import page_predict_tenure_body
-from app_pages.page_cluster import page_cluster_body
 
-app = MultiPage(app_name= "Churnometer") # Create an instance of the app
+
+app = MultiPage(app_name= "House Price Predictor") # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Customer Base Churn Study", page_churned_customer_study_body)
-app.add_page("Prospect Churnometer", page_prospect_body)
-app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-app.add_page("ML: Prospect Churn", page_predict_churn_body)
-app.add_page("ML: Prospect Tenure", page_predict_tenure_body)
-app.add_page("ML: Cluster Analysis", page_cluster_body)
+# app.add_page("House Price Characteristics Study", page_churned_customer_study_body)
+# app.add_page("Houseprice Predictor Tool", page_houseprices_predictor_tool_body)
+# app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+# app.add_page("ML: Regression Analysis", page_regression_analysis_body)
+# app.add_page("ML: Data Cleaning and Feature Engineering", page_data_cleaning_and_feature_engineering_body)
 
 app.run() # Run the app
---
+++ app_pages/page_summary.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+def page_summary_body():
+
+    st.write("# Summary of a House Price Predictor for Ames, Iowa")
+
+    # text based on README file - "Dataset Content" section
+    st.info(
+        f"**Project Terms & Jargon**\n"
+        f"* A **house** is a property in Ames, Iowa, irrespective of its characteristics.\n"
+        f"* **Sale price** is the price which was paid when the house was sold.\n"
+        f"* **Characteristics** refer to both objective and subjectivedescriptions"
+        f"of the house. Objective characteristics include space in square feet, number"
+        f"of bedrooms, etc, whilst subjective include judgment of overall quality and"
+        f"condition of the house, quality of the kitchen, etc.\n "
+        f"**Project Dataset**\n"
+        f"* The dataset is a publicly available record of 1,460 house sales in Ames, Iowa,"
+        f"containing a range of information about each house sold, including"
+        f"the sale price. The information available does not provide a complete picture"
+        f"of each house sold, with certain pertinent information not included, such as"
+        f"location and neighbourhood characteristics, date of sale, and other factors"
+        f"likely to affect house prices.\n"
+        f"Nonetheless, there is sufficient information available in the dataset for"
+        f"the Houseprice Predictor tool to provide accurate estimates for house prices"
+        f"based on the available characteristics."
+        )
+
+    # Link to README file, so the users can have access to full project documentation
+    st.write(
+        f"* Additional information about the Houseprice Predictor app can be"
+        f"found in the"
+        f"[Project README file](https://github.com/Code-Institute-Solutions/churnometer).")    
+
+    # copied from README file - "Business Requirements" section
+    st.success(
+        f"The project has 3 business requirements:\n"
+        f"* **1** - The **client** is interested in discovering how the characteristics of houses correlate with the sale price." 
+        f"Therefore, the client expects data visualisations of the correlated variables against the sale price to show how"
+        f"they are linked.\n"
+        f"* **2** - The client is interested in predicting the house sale price for her four inherited houses.\n"
+        f"* **3** - The client would also like to be able to estimate sale prices for other houses in Ames, Iowa, based on"
+        f"the relevant and pertinent characteristics identified through the Machine Learning process."
+        )
